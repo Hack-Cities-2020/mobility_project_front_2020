346b652f3b068f4222520665bf44146497120ea3
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-29 10:22:33 -0400

    basic map with marker

---
+++ src/components/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+    >
+    <GmapMarker
+      :key="index"
+      v-for="(marker, index) in markers"
+      :position="marker.position"
+      :icon="marker.icon"
+      :clickable="true"
+      :draggable="true"
+      @click="center=marker.position"
+    />
+  </GmapMap>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  data: () => ({
+    markers: [
+      { position: {lat:10, lng:10}, icon: { url: "/assets/checkpoint_marker.svg" } }
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
