202a41b2a222b8375aeb0d6ea0f7bb6afea972eb
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-29 17:51:04 -0400

    bounds, and center on la paz

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,5 +1,6 @@
 <template>
   <GmapMap
+    ref="map"
     :center="center"
     :zoom="zoom"
     :style="{ width: width, height: height }"
@@ -10,15 +11,32 @@
 </template>
 
 <script>
+import {gmapApi} from 'vue2-google-maps'
+
 export default {
   name: "BaseMap",
   props: {
     width: { type: String, default: "100%" },
     height: { type: String, default: "400px" },
     zoom: { type: Number, default: 10 },
-    center: { type: Object, default: () => ({lat: -16.6143771, lng: -64.1916404}) }
+    bounds: { type: Array, default: () => [] },
+    center: { type: Object, default: () => ({lat: -16.5, lng: -68.15}) }
   },
   data: () => ({}),
+  computed: {
+    google: gmapApi
+  },
+  mounted() {
+    if (this.bounds.length) {
+      this.$refs.map.$mapPromise.then(map => {
+        var bounds = new this.google.maps.LatLngBounds();
+        this.bounds.forEach(point => {
+          bounds.extend(point);
+        });
+        map.fitBounds(bounds);
+      });
+    }
+  }
 }
 </script>
 
