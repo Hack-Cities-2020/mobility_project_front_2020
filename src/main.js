518282a247c99ac7b0d69c27c52024746c1aa0fe
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-29 15:38:59 -0400

    readded main.js

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_MAPS_TOKEN,
+  },
+  installComponents: true
+});
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
