23f2849f615af12c156ea69064668f783b4af819
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-29 14:05:54 -0400

    base map with default values

---
+++ src/components/BaseMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <GmapMap
+    :center="center"
+    :zoom="zoom"
+    :style="{ width: width, height: height }"
+    @click="event => $emit('click', event)"
+  >
+    <slot></slot>
+  </GmapMap>
+</template>
+
+<script>
+export default {
+  name: "BaseMap",
+  props: {
+    width: { type: String, default: "100%" },
+    height: { type: String, default: "500px" },
+    zoom: { type: Number, default: 10 },
+    center: { type: Object, default: () => ({lat: -16.6143771, lng: -64.1916404}) }
+  },
+  data: () => ({}),
+}
+</script>
+
+<style>
+
+</style>
