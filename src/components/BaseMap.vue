70cec873c6583042e438f5ea018ed35847388a9a
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-29 16:27:52 -0400

    taller map

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -14,7 +14,7 @@ export default {
   name: "BaseMap",
   props: {
     width: { type: String, default: "100%" },
-    height: { type: String, default: "500px" },
+    height: { type: String, default: "400px" },
     zoom: { type: Number, default: 10 },
     center: { type: Object, default: () => ({lat: -16.6143771, lng: -64.1916404}) }
   },
