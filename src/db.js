cc3268279d80792d65ccec2965f5d8c122762634
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-31 13:36:57 -0400

    firebase config

--- src/db.js
+++ src/db.js
@@ -3,9 +3,17 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-export const db = firebase
-    .initializeApp({ projectId: 'hackcities2020-lapaz' })
-    .firestore()
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_TOKEN,
+  authDomain: "hackcities2020-lapaz.firebaseapp.com",
+  databaseURL: "https://hackcities2020-lapaz.firebaseio.com",
+  projectId: "hackcities2020-lapaz",
+  storageBucket: "hackcities2020-lapaz.appspot.com",
+  messagingSenderId: "553853632033",
+  appId: "1:553853632033:web:6d02910ce126cfbe3fed60"
+};
+
+export const db = firebase.initializeApp(firebaseConfig).firestore()
 
 
 // Export types that exists in Firestore - Uncomment if you need them in your app
