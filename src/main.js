1837b10cb31022126247b2e794f387a82e53306d
Hernando Calle Valencia <hernando_valencia@icloud.com>
2020-03-31 11:37:01 -0400

    added vuefire

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import * as VueGoogleMaps from 'vue2-google-maps';
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(firestorePlugin)
 
 Vue.use(VueGoogleMaps, {
   load: {
